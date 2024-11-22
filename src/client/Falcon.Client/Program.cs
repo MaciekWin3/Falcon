b6fa901584f7875e9e5c4d55ce4663b462555471
Maciej Winnik <maciekwin3@gmail.com>
2024-11-22 17:29:13 +0100

    Add config file to setup app

--- src/client/Falcon.Client/Program.cs
+++ src/client/Falcon.Client/Program.cs
@@ -1,65 +1,60 @@
-﻿using Falcon.Client.Features.Auth;
+﻿using Falcon.Client;
+using Falcon.Client.Features.Auth;
 using Falcon.Client.Features.Auth.UI;
 using Falcon.Client.Features.Chat;
 using Falcon.Client.Features.Chat.UI;
 using Falcon.Client.Features.Lobby.UI;
 using Falcon.Client.Features.SignalR;
+using Falcon.Client.Utils;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Falcon.Client
-{
-    internal class Program
+var builder = new ConfigurationBuilder();
+BuildConfig(builder);
+var configuration = builder.Build();
+
+ConfigHelper.CreateConfig();
+var config = ConfigHelper.GetConfig();
+
+var host = Host.CreateDefaultBuilder()
+    .ConfigureServices((context, services) =>
     {
-        private static async Task Main()
+        // Services
+        services.AddScoped<SignalRClient>();
+        services.AddTransient<ChatService>();
+        services.AddTransient<AuthService>();
+        services.AddTransient<TerminalOrchestrator>();
+
+        // Windows
+        services.AddTransient<ChatWindow>();
+        services.AddTransient<LoginWindow>();
+        services.AddTransient<LobbyWindow>();
+
+        services.AddHttpClient("Server", client =>
         {
-            var builder = new ConfigurationBuilder();
-            BuildConfig(builder);
-            var configuration = builder.Build();
+            client.BaseAddress = new Uri(configuration["ServerIp"]);
+        });
 
-            var host = Host.CreateDefaultBuilder()
-                .ConfigureServices((context, services) =>
-                {
-                    // Services
-                    services.AddScoped<SignalRClient>();
-                    services.AddTransient<ChatService>();
-                    services.AddTransient<AuthService>();
-                    services.AddTransient<TerminalOrchestrator>();
+        services.AddLogging(builder =>
+            {
+                builder
+                    .AddFilter("Microsoft", LogLevel.Warning)
+                    .AddFilter("System", LogLevel.Warning)
+                    .AddFilter("NToastNotify", LogLevel.Warning)
+                    .AddConsole();
+            }
+        );
+    })
+    .Build();
 
-                    // Windows
-                    services.AddTransient<ChatWindow>();
-                    services.AddTransient<LoginWindow>();
-                    services.AddTransient<LobbyWindow>();
+var svc = ActivatorUtilities.CreateInstance<TerminalOrchestrator>(host.Services);
+svc.InitApp();
 
-                    services.AddHttpClient("Server", client =>
-                    {
-                        client.BaseAddress = new Uri(configuration["ServerIp"]);
-                    });
-
-                    services.AddLogging(builder =>
-                        {
-                            builder
-                                .AddFilter("Microsoft", LogLevel.Warning)
-                                .AddFilter("System", LogLevel.Warning)
-                                .AddFilter("NToastNotify", LogLevel.Warning)
-                                .AddConsole();
-                        }
-                    );
-                })
-                .Build();
-
-            var svc = ActivatorUtilities.CreateInstance<TerminalOrchestrator>(host.Services);
-            svc.InitApp();
-        }
-
-        private static void BuildConfig(IConfigurationBuilder builder)
-        {
-            builder.SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                //.AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json")
-                .AddEnvironmentVariables();
-        }
-    }
+void BuildConfig(IConfigurationBuilder builder)
+{
+    builder.SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddEnvironmentVariables();
 }
